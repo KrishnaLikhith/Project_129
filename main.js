50284fa0a25f9b76f25e9e2a85651cda0b194cbf
KrishnaLikhith <76393325+KrishnaLikhith@users.noreply.github.com>
2021-10-16 15:37:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+believer = "";
+phoenix = "";
+leftwX = 0;
+leftwY = 0;
+rightwX = 0;
+rightwY = 0;
+songPlaying = "";
+scorel = 0;
+scorer = 0;
+
+function preload(){
+    believer = loadSound("Believer.mp3");
+    phoenix = loadSound("Phoenix.mp3");
+    
+}
+function setup(){
+canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPosses);
+}
+ function draw(){
+     image(video, 0,0,600,500);
+     believer.isPlaying();
+     
+     if(scorel > 0.2){
+         circle(leftwX,leftwY,10);
+         fill("#FF0000");
+         stroke("#FF0000");
+         phoenix.stop();
+         if(believer == false){
+             believer.isPlaying();
+             document.getElementById("song").innerHTML = "Song Playing - Believer";
+         }
+
+     }
+     
+ }
+function modelLoaded(){
+    console.log("Model is Loaded");
+}
+function gotPosses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftwX = results[0].pose.leftWrist.x;
+        leftwY = results[0].pose.leftWrist.y;
+        rightwX = results[0].pose.rightWrist.x;
+        rightwY = results[0].pose.rightWrist.y;
+        scorel = results[0].pose.keypoints[9].score;
+        scorer = results[0].pose.keypoints[10].score;
+    }
+}   
